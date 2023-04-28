a478541922eddafd270015ec6617c4128917e317
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-04-28 15:33:42 -0700

    0. I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ _printf.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string
+ *
+ * Return: total of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int total = 0;
+	char *ptr, *from;
+	va_list args;
+
+	va_start(args, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (ptr = (char *)format; *ptr; ptr++)
+	{
+		if (*ptr != '%')
+		{
+			total += _putchar(*ptr);
+			continue;
+		}
+		from = ptr;
+		ptr++;
+		if (!get_specifier(ptr))
+			total += print_from_to(from, ptr);
+		else
+			total += get_print_func(ptr, args);
+	}
+	_putchar(-1);
+	va_end(args);
+	return (total);
+}
