03c47025e16fd2c770e5de44c664cfc1d129e42b
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-04-28 15:51:50 -0700

    fin a finction related to given spcifier

---
+++ 2-specifier.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * get_specifier - find function related to given spcifier
+ * @s: format specifier
+ *
+ * Return: function pointer or NULL if not found
+ */
+int (*get_specifier(char *s))()
+{
+	specifier specifiers[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}};
+	int i = 0;
+
+	while (specifiers[i].specifier)
+	{
+		if (*s == specifiers[i].specifier[0])
+		{
+			return (specifiers[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+/**
+ * get_print_func - call function print of given specifier
+ * @s: format specifier
+ * @args: pointer to arguments of variadic function
+ * Return: print_function pointer or 0 if NULL
+ */
+int get_print_func(char *s, va_list args)
+{
+	int (*f)(va_list) = get_specifier(s);
+
+	if (f)
+		return (f(args));
+	return (0);
+}
