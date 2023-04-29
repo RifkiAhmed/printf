632efe1382679b66a33f022a4b499846562114ae
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-04-28 19:24:09 -0700

    print a strig from a start position to stop position

--- 4-printers.c
+++ 4-printers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * print_from_to - prints a string
+ * print_from_to - a function that  prints a string
  * @from: start of print
  * @to: stop of print
  *
  * Return: number of character printed
  */
+
 int print_from_to(char *from, char *to)
 {
 	int sum = 0;
